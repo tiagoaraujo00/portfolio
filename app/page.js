008a30566516fd22010a0279116e546c71bff152
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-21 04:46:34 -0300

    feat: hero section implementation

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import HeroSection from "./components/HeroSection";
+
+
+export default function Home() {
+  return <main className="flex flex-col min-h-screen bg-[#121212]">
+    <div class="container mx-auto px-12 py-4">
+      <HeroSection />
+    </div>
+  </main>;
+}
