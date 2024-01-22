49ce92dbf6420e0eb818276007c8cbdaf349e0a7
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-21 23:23:22 -0300

    feat: navbar

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font font-extrabold">
+          <h1 className="text-white mb-4 text-3xl sm:text-5xl lg:text-6xl font font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               Hello, I am{" "}
             </span>
@@ -25,7 +25,6 @@ const HeroSection = () => {
               ]}
               wrapper="span"
               speed={50}
-              
               repeat={Infinity}
             />
           </h1>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import HeroSection from "./components/HeroSection";
-
+import Navbar from "./components/Navbar";
 
 export default function Home() {
-  return <main className="flex flex-col min-h-screen bg-[#121212]">
-    <div class="container mx-auto px-12 py-4">
-      <HeroSection />
-    </div>
-  </main>;
+  return (
+    <main className="flex flex-col min-h-screen bg-[#121212]">
+      <Navbar />
+      <div class="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
+    </main>
+  );
 }
