9b09b40fbc58ef97c0cb59f19bd06d661c306038
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-29 02:28:38 -0300

    feat: resend package

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const EmailSection = () => {
+  return (
+    <section className="grid gap-4 py-24 my-12 md:grid-cols-2 md:my12">
+      <div>
+        <h5 className="my-2 text-xl font-bold text-white">Let's Connect</h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
+          {" "}
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
