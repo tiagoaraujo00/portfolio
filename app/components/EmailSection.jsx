3c1884d0c2b68735baeb702820ce59004815ca26
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-30 22:35:51 -0300

    style: responsive image design

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -26,7 +26,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <Image className="ml-48 rounded-md" src="/images/message.jpg" width={400} height={400} />
+        <Image className="rounded-md md:ml-48" src="/images/message.jpg" width={400} height={400} />
         {/* <form className="flex flex-col">
           <div className="mb-6">
             <label
