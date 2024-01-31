f36345cea11e52f68c145d17428510248421d09a
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-31 14:15:48 -0300

    style: footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className='container flex justify-between p-3'>
         <Image className='ml-8' src="/images/brain.png" alt="logo" width={50} height={50}/>
-        <span className='mt-5 text-slate-600'>All rights reserved.</span>
+        <span className='mt-4 text-slate-600'>All rights reserved.</span>
       </div>
     </footer>
   )
